12396883791bf0222aab33e4658f8e676cb43872
unknown <Mikhail.Zarechenskiy@UNIT-422.Labs.IntelliJ.Net>
2013-05-30 12:55:56 +0400

    Fix word detector for keyword highlightning

--- kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/WordDetector.java
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/editors/WordDetector.java
@@ -3,13 +3,27 @@ package org.jetbrains.kotlin.ui.editors;
 import org.eclipse.jface.text.rules.IWordDetector;
 
 public class WordDetector implements IWordDetector {
+    
+    private char previous = Character.SPACE_SEPARATOR;
+    
     @Override
     public boolean isWordStart(char c) {
-        return Character.isJavaIdentifierStart(c);
+        if (Character.isLetter(previous) || Character.isDigit(previous)) {
+            previous = c;
+            
+            return false;
+        }
+        previous = c;
+        
+        return isKotlinIdentifierStart(c);
     }
 
     @Override
     public boolean isWordPart(char c) {
         return Character.isJavaIdentifierPart(c);
+    }
+    
+    private boolean isKotlinIdentifierStart(char c) {
+        return Character.isLetter(c);
     }
 }
