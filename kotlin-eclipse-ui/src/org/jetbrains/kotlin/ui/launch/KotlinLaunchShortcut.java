0b44d1e8e115ea5b087da8f3bc623c35a5c41938
zarechenskiy <rostov93@gmail.com>
2013-06-26 15:29:39 +0400

    Tempate for implementation of kotlin launch shortcut

---
+++ kotlin-eclipse-ui/src/org/jetbrains/kotlin/ui/launch/KotlinLaunchShortcut.java
@@ -0,0 +1,17 @@
+package org.jetbrains.kotlin.ui.launch;
+
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+public class KotlinLaunchShortcut implements ILaunchShortcut {
+
+    @Override
+    public void launch(ISelection selection, String mode) {
+    }
+
+    @Override
+    public void launch(IEditorPart editor, String mode) {
+    }
+
+}
