c27d472859b42fc178b9eac163f053a766a02795
zarechenskiy <rostov93@gmail.com>
2013-06-27 17:16:30 +0400

    Improve launch configuration delegate with other api

--- kotlin-eclipse-core/src/org/jetbrains/kotlin/core/launch/LaunchConfigurationDelegate.java
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/launch/LaunchConfigurationDelegate.java
@@ -1,57 +1,82 @@
 package org.jetbrains.kotlin.core.launch;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IVMRunner;
 import org.eclipse.jdt.launching.VMRunnerConfiguration;
 import org.jetbrains.jet.cli.jvm.K2JVMCompiler;
-import org.jetbrains.jet.lang.resolve.java.PackageClassUtils;
-import org.jetbrains.jet.lang.resolve.name.FqName;
 import org.jetbrains.kotlin.core.builder.KotlinManager;
 import org.jetbrains.kotlin.core.utils.ProjectUtils;
+import org.osgi.framework.Bundle;
 
 public class LaunchConfigurationDelegate extends AbstractJavaLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {    
+
+    private static String ktHome = "";
     
-    // TODO: Change to independent system
-    private final String ktHome = "C:/Users/Mikhail.Zarechenskiy/projects/kotlin-eclipse/kotlin-bundled-compiler/"; 
+    static {
+        Bundle bundle = Platform.getBundle("org.jetbrains.kotlin.ui");
+        Path iconPath = new Path("icons/sample.gif");
+        try {
+            Path path = new Path(FileLocator.resolve(FileLocator.find(bundle, iconPath, null)).getPath());
+            
+            ktHome = path.removeLastSegments(3).toPortableString() + "kotlin-bundled-compiler/";
+        } catch (IOException e) {
+        }
+    }
     private final String ktCompiler = "kotlin-compiler-0.5.162.jar";
-    private final String defaultPackage = "_DefaultPackage"; 
      
     @Override
-    public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
-            throws CoreException {
+    public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
         
         if (monitor == null) {
             monitor = new NullProgressMonitor();
         }
-
+        
         compileKotlinFiles(KotlinManager.getAllFiles().toArray(new IFile[KotlinManager.getAllFiles().size()]), configuration);
         
         IVMRunner runner = getVMRunner(configuration, mode);
         
-        String packageClassName = ProjectUtils.getMainPackage();
-        if (packageClassName.equals("")) {
-            packageClassName = defaultPackage;
-        } else {
-            packageClassName += "." + PackageClassUtils.getPackageClassName(new FqName(packageClassName));
+        try {
+            String packageClassName = getPackageClassName(configuration);
+            VMRunnerConfiguration vmConfig = new VMRunnerConfiguration(packageClassName, new String[] {getOutputDir(configuration)});
+            runner.run(vmConfig, launch, monitor);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Wrong configuration");
         }
         
-        VMRunnerConfiguration vmConfig = new VMRunnerConfiguration(packageClassName, new String[] {getOutputDir(configuration)});
-        runner.run(vmConfig, launch, monitor);
-        
         monitor.done();
+    }
+    
+    private String getPackageClassName(ILaunchConfiguration configuration) {
+        String mainClass = "";
+        try {
+            mainClass = configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, (String)null);
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+        
+        for (IFile file : KotlinManager.getAllFiles()) {
+            if (file.getName().equals(mainClass)) {
+                if (ProjectUtils.getMainClass(Arrays.asList(file)) != null) {
+                    return ProjectUtils.createPackageClassName(file);
+                }
+            }
+        }
+        
+        throw new IllegalArgumentException();
     }
     
     private void compileKotlinFiles(IFile[] files, ILaunchConfiguration configuration) {
@@ -67,19 +92,7 @@ public class LaunchConfigurationDelegate extends AbstractJavaLaunchConfiguration
         
         command.append(" -output " + getOutputDir(configuration));
         try {
-            System.out.println("Building...");
-            
-            Process process = Runtime.getRuntime().exec(command.toString());
-            BufferedReader br = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            
-            System.out.println("Done.");
-            
-            process.waitFor();
-            //Runtime.getRuntime().exec(command.toString()).waitFor();
+            Runtime.getRuntime().exec(command.toString()).waitFor();
         } catch (IOException | InterruptedException e) {
             System.out.println("Exception: " + e.getMessage());
         }        
@@ -98,21 +111,4 @@ public class LaunchConfigurationDelegate extends AbstractJavaLaunchConfiguration
         
         return ".";
     }
-    
-    private String getClassToLaunch(ILaunchConfiguration configuration) throws CoreException {
-        return verifyMainTypeName(configuration);
-    }
-    
-    private String[] getClassPath(ILaunchConfiguration configuration) throws CoreException {
-        List<String> paths = new ArrayList<String>();
-        
-        for (String string : getClasspath(configuration)) {
-            System.out.println("cp: " + string);
-            paths.add(string);
-        }
-                
-        String[] result = new String[paths.size()];
-        return paths.toArray(result);
-    }
-    
 }
