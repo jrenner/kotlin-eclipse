6a83e8ec4e87a8fa61146ce4d27ed3cc36d0b024
zarechenskiy <rostov93@gmail.com>
2013-06-28 15:42:28 +0400

    Add utility class to work with packages

---
+++ kotlin-eclipse-core/src/org/jetbrains/kotlin/core/utils/ProjectUtils.java
@@ -0,0 +1,31 @@
+package org.jetbrains.kotlin.core.utils;
+
+import java.util.Collection;
+
+import org.eclipse.core.resources.IFile;
+import org.jetbrains.jet.lang.resolve.java.PackageClassUtils;
+import org.jetbrains.jet.lang.resolve.name.FqName;
+import org.jetbrains.jet.plugin.JetMainDetector;
+
+public class ProjectUtils {
+    
+    public static IFile getMainClass(Collection<IFile> files) {
+        KotlinEnvironment kotlinEnvironment = new KotlinEnvironment();
+        
+        for (IFile file : files) {
+            if (JetMainDetector.hasMain(kotlinEnvironment.getJetFile(file).getDeclarations())) {
+                return file;
+            }
+        }
+                
+        return null;
+    }
+    
+    public static String getPackageByFile(IFile file) {
+        return new KotlinEnvironment().getJetFile(file).getPackageName();
+    }
+    
+    public static String createPackageClassName(IFile file) {
+        return PackageClassUtils.getPackageClassFqName(new FqName(getPackageByFile(file))).toString();
+    }
+}
